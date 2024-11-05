6d73ee10669dd3a245400a4d4e1a2e9ac709bae5
Faisal Waris <FaisalWaris@live.com>
2024-11-05 18:03:09 -0500

    test audio

--- src/RTOpenAI/scripts/AudioProps.fsx
+++ src/RTOpenAI/scripts/AudioProps.fsx
@@ -10,3 +10,29 @@ r1.[0].Tags |> Seq.iter (fun x -> printfn "%A" x)
 
 44100. * 10./ 8.;;
 
+#r "nuget: Silk.NET.Core, 2.22.0"
+#r "nuget: Silk.NET.OpenAL, 2.22.0"
+#r "nuget: Silk.NET.OpenAL.Soft.Native, 1.23.1"
+#r "nuget: Silk.NET.OpenAL.Extensions.Soft, 2.22.0"
+open Silk.NET.OpenAL
+open Silk.NET.Core.Loader
+
+let sampleRate = 22050
+let byteRate = sampleRate * 2
+let format = BufferFormat.Mono16
+let blockAlign = 2
+let bits = System.IO.File.ReadAllBytes(@"C:\Users\Faisa\Music\PinkPanther30 - Copy.pcm")
+
+let alc = ALContext.GetApi()
+let al = AL.GetApi()
+let device = alc.OpenDevice("")
+let _ =  context.CreateContext(device, NativeInterop.NativePtr.nullPtr)
+
+let buffer = al.GenBuffer()
+let source = al.GenSource()
+
+
+
+
+
+
