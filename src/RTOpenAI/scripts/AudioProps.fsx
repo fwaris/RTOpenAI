e8a3f08405d5476e9122920aee41d7da4ece7f04
Faisal Waris <FaisalWaris@live.com>
2024-11-05 22:41:42 -0500

    audio sample

--- src/RTOpenAI/scripts/AudioProps.fsx
+++ src/RTOpenAI/scripts/AudioProps.fsx
@@ -1,11 +1,42 @@
-#r "nuget: NAudio.Core"
-
+#r "nuget: Silk.NET.OpenAL"
+#r "nuget: Silk.NET.OpenAL.Soft.Native"
+#r "nuget: Silk.NET.OpenAL.Extensions.Soft"
 open System
-open NAudio.Wave
+open Microsoft.FSharp.NativeInterop
+open Silk.NET.OpenAL
+open System.IO
 
-let testFile = @"C:\Users\Faisa\AppData\Local\Packages\5D26BACE-A3ED-4E42-ABDA-1B0C073F7763_9zz4h110yvjzm\LocalCache\o551an5p.sog.wav"
+let wav = File.ReadAllBytes(@"/Users/faisalwaris/Downloads/PinkPanther30.wav").[44..]
+let sampleRate = 22050
+let format = BufferFormat.Mono16
+
+let alc = ALContext.GetApi();
+let al = AL.GetApi();
+let device = alc.OpenDevice("")
+let nullPtr = NativeInterop.NativePtr.nullPtr
+let context = alc.CreateContext(device, nullPtr);
+alc.MakeContextCurrent(context);
+
+al.GetError();
+
+let source = al.GenSource();
+let buffer = al.GenBuffer();
+al.SetSourceProperty(source, SourceBoolean.Looping, true)
+al.BufferData(buffer, format, wav, sampleRate)
 
 
-let wave = new WaveFileReader(testFile)
-wave.WaveFormat
-wave.Close()
+al.SetSourceProperty(source, SourceInteger.Buffer, buffer);
+al.SourcePlay(source);
+
+Console.WriteLine("Press Enter to Exit...");
+Console.ReadLine();
+
+al.SourceStop(source);
+
+al.DeleteSource(source);
+al.DeleteBuffer(buffer);
+alc.DestroyContext(context);
+alc.CloseDevice(device);
+al.Dispose();
+alc.Dispose();
+
