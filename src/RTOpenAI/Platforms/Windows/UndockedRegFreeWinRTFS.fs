fa2baf04b4e43ea15dc2d63a3e6478863de73073
Faisal Waris <FaisalWaris@live.com>
2024-11-22 11:44:02 -0500

    reg file code

---
+++ src/RTOpenAI/Platforms/Windows/UndockedRegFreeWinRTFS.fs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.Windows.Foundation.UndockedRegFreeWinRTFS
+
+open System.Runtime.InteropServices
+
+module private NativeMethods =
+
+    [<DllImport("Microsoft.WindowsAppRuntime.dll", CharSet = CharSet.Unicode, ExactSpelling = true)>]
+    extern int WindowsAppRuntime_EnsureIsLoaded()
+
+[<AbstractClass; Sealed>]
+type Initializer =
+
+    static member AccessWindowsAppSDK () =
+        NativeMethods.WindowsAppRuntime_EnsureIsLoaded ()
+        |> ignore // return value not required
